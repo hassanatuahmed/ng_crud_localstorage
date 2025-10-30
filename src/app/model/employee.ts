5df3176b870b5a568af3321b64c6196c28f007c0
Hassanatu Ahmed <hassanatuahmed05@gmail.com>
2025-10-30 14:58:18 +0000

    building new employee forms

---
+++ src/app/model/employee.ts
@@ -0,0 +1,20 @@
+export class EmployeeModel{
+employeeId: number;
+name: string;
+city: string;
+state: string;
+emailId: string;
+contactNo: string;
+address: string;
+pinCode: string;
+constructor(){
+    this.address = '';
+    this.city = '';
+    this.contactNo = '';
+    this.emailId = '';
+    this.name = '';
+    this.state = '';
+    this.employeeId = 0;
+    this.pinCode='';
+}
+}
