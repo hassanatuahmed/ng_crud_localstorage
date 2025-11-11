6c6eb4794eaf79360e91f6bb41b8a53abf7766bd
Hassanatu Ahmed <hassanatuahmed05@gmail.com>
2025-11-11 12:19:43 +0000

    building forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,54 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/employee';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ng_crud_localstorage');
+  employeeForm: FormGroup = new FormGroup({});
+  employeeObj: EmployeeModel = new EmployeeModel();
+  employeeList: EmployeeModel[] = [];
+
+  constructor() {
+    this.createForm();
+    debugger;
+    const oldData = localStorage.getItem("EmpData");
+    if (oldData != null) {
+      const parseData = JSON.parse(oldData);
+      this.employeeList = parseData;
+    }
+  }
+
+  createForm() {
+    this.employeeForm = new FormGroup({
+      employeeId: new FormControl(this.employeeObj.employeeId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+
+      state: new FormControl(this.employeeObj.state),
+      emailId: new FormControl(this.employeeObj.emailId),
+      contactNo: new FormControl(this.employeeObj.contactNo),
+      address: new FormControl(this.employeeObj.address),
+      pinCode: new FormControl(this.employeeObj.pinCode),
+    })
+
+  }
+
+  onSave() {
+    debugger;
+    const oldData = localStorage.getItem("EmpData");
+    if (oldData != null) {
+      const parseData = JSON.parse(oldData);
+      this.employeeForm.controls['empid'].setValue(parseData.length + 1);
+      this.employeeList.unshift(this.employeeForm.value);
+    } else {
+      this.employeeList.unshift(this.employeeForm.value);
+    }
+  }
 }
