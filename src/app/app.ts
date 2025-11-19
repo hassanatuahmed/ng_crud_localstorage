fb5439e1cc4adc5299b277ec6b953fe01cde3890
Hassanatu Ahmed <hassanatuahmed05@gmail.com>
2025-11-19 14:40:29 +0000

    addes appwrite

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,120 +1,161 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { EmployeeModel } from './model/employee';
+// import { Component, OnInit, signal } from '@angular/core';
+// import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { RouterOutlet } from '@angular/router';
+// import { EmployeeModel } from './model/employee';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [ReactiveFormsModule],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App implements OnInit {
+//   protected readonly title = signal('ng_crud_localstorage');
+//   employeeForm: FormGroup = new FormGroup({});
+//   employeeObj: EmployeeModel = new EmployeeModel();
+//   employeeList: EmployeeModel[] = [];
+//   isEditMode = false;
+
+//   ngOnInit() {
+//     const data = localStorage.getItem("EmpData");
+//     this.employeeList = data ? JSON.parse(data) : [];
+//   }
+
+//   constructor() {
+//     this.createForm();
+//     const oldData = localStorage.getItem("EmpData");
+//     if (oldData != null) {
+//       const parseData = JSON.parse(oldData);
+//       this.employeeList = parseData;
+//     }
+//   }
+
+//   createForm() {
+//     this.employeeForm = new FormGroup({
+//       employeeId: new FormControl('', [Validators.required]),
+//       name: new FormControl('', Validators.required),
+//       city: new FormControl(''),
+
+//       state: new FormControl(''),
+//       emailId: new FormControl('', Validators.email),
+//       contactNo: new FormControl('', Validators.maxLength(10)),
+//       address: new FormControl(''),
+//       pinCode: new FormControl(''),
+//     })
+
+//   }
+
+//   reset() {
+//     this.employeeForm.reset();
+//   }
+
+//   onSave() {
+
+//     const oldData = localStorage.getItem("EmpData");
+//     if (oldData != null) {
+//       const parseData = JSON.parse(oldData);
+//       this.employeeForm.controls['employeeId'].setValue(parseData.length + 1);
+//       this.employeeList.unshift(this.employeeForm.value);
+//     } else {
+//       this.employeeList.unshift(this.employeeForm.value);
+//     }
+//     localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+//     alert("Employee saved successfully!");
+//     this.onAddNew();
+
+
+
+//   }
+//   onAddNew() {
+//     this.isEditMode = false;
+//     this.employeeForm.reset();
+//   }
+
+
+//   onEdit(item: EmployeeModel) {
+//     this.isEditMode = true;
+//     this.employeeForm.patchValue(item);
+
+//   }
+
+//   onUpdate() {
+//     const record = this.employeeList.find(m =>
+//       m.employeeId == this.employeeForm.controls['employeeId'].value);
+//     if (record != undefined) {
+//       record.address = this.employeeForm.controls['address'].value;
+//       record.city = this.employeeForm.controls['city'].value;
+//       record.contactNo = this.employeeForm.controls['contactNo'].value;
+//       record.emailId = this.employeeForm.controls['emailId'].value;
+//       record.employeeId = this.employeeForm.controls['address'].value;
+//       record.name = this.employeeForm.controls['name'].value;
+//       record.pinCode = this.employeeForm.controls['pinCode'].value;
+//       record.state = this.employeeForm.controls['state'].value;
+
+//     }
+//     localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+//     this.reset();
+
+//   }
+
+//   onDelete(id: number) {
+//     console.log("hello");
+//     const isDelete = confirm("Are you sure you want to delete");
+//     if (isDelete) {
+//       const index = this.employeeList.findIndex(m => m.employeeId === Number(id));
+//       if (index !== -1) {
+//         this.employeeList.splice(index, 1);
+//         this.onAddNew();
+//         localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+
+//         this.employeeList = JSON.parse(localStorage.getItem("EmpData") || "[]");
+
+//       } else {
+//         alert("Employee not found!");
+//       }
+
+//     }
+//   }
+
+
+// }
+
+
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {tuiAsPortal, TuiPortals, TuiRepeatTimes} from '@taiga-ui/cdk';
+import {TuiAppearance, TuiButton, TuiDropdownService, TuiTitle} from '@taiga-ui/core';
+import {TuiChevron, TuiFade} from '@taiga-ui/kit';
+import {
+    TuiCardLarge,
+    TuiHeader,
+    tuiLayoutIconsProvider,
+    TuiNavigation,
+} from '@taiga-ui/layout';
 
 @Component({
-  selector: 'app-root',
-  imports: [ReactiveFormsModule],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    standalone: true,
+    exportAs: "Example4",
+    imports: [
+        TuiAppearance,
+        TuiButton,
+        TuiCardLarge,
+        TuiChevron,
+        TuiFade,
+        TuiHeader,
+        TuiNavigation,
+        TuiRepeatTimes,
+        TuiTitle,
+    ],
+   templateUrl: './app.html',
+    styleUrls: ['./app.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [
+        tuiLayoutIconsProvider({grid: '@tui.align-justify'}),
+        // Ignore portal related code, it is only here to position drawer inside the example block
+        TuiDropdownService,
+        tuiAsPortal(TuiDropdownService),
+    ],
 })
-export class App implements OnInit {
-  protected readonly title = signal('ng_crud_localstorage');
-  employeeForm: FormGroup = new FormGroup({});
-  employeeObj: EmployeeModel = new EmployeeModel();
-  employeeList: EmployeeModel[] = [];
-  isEditMode = false;
-
-  ngOnInit() {
-    const data = localStorage.getItem("EmpData");
-    this.employeeList = data ? JSON.parse(data) : [];
-  }
-
-  constructor() {
-    this.createForm();
-    const oldData = localStorage.getItem("EmpData");
-    if (oldData != null) {
-      const parseData = JSON.parse(oldData);
-      this.employeeList = parseData;
-    }
-  }
-
-  createForm() {
-    this.employeeForm = new FormGroup({
-      employeeId: new FormControl('', [Validators.required]),
-      name: new FormControl('', Validators.required),
-      city: new FormControl(''),
-
-      state: new FormControl(''),
-      emailId: new FormControl('', Validators.email),
-      contactNo: new FormControl('', Validators.maxLength(10)),
-      address: new FormControl(''),
-      pinCode: new FormControl(''),
-    })
-
-  }
-
-  reset() {
-    this.employeeForm.reset();
-  }
-
-  onSave() {
-
-    const oldData = localStorage.getItem("EmpData");
-    if (oldData != null) {
-      const parseData = JSON.parse(oldData);
-      this.employeeForm.controls['employeeId'].setValue(parseData.length + 1);
-      this.employeeList.unshift(this.employeeForm.value);
-    } else {
-      this.employeeList.unshift(this.employeeForm.value);
-    }
-    localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
-    alert("Employee saved successfully!");
-    this.onAddNew();
-
-
-
-  }
-  onAddNew() {
-    this.isEditMode = false;
-    this.employeeForm.reset();
-  }
-
-
-  onEdit(item: EmployeeModel) {
-    this.isEditMode = true;
-    this.employeeForm.patchValue(item);
-
-  }
-
-  onUpdate() {
-    const record = this.employeeList.find(m =>
-      m.employeeId == this.employeeForm.controls['employeeId'].value);
-    if (record != undefined) {
-      record.address = this.employeeForm.controls['address'].value;
-      record.city = this.employeeForm.controls['city'].value;
-      record.contactNo = this.employeeForm.controls['contactNo'].value;
-      record.emailId = this.employeeForm.controls['emailId'].value;
-      record.employeeId = this.employeeForm.controls['address'].value;
-      record.name = this.employeeForm.controls['name'].value;
-      record.pinCode = this.employeeForm.controls['pinCode'].value;
-      record.state = this.employeeForm.controls['state'].value;
-
-    }
-    localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
-    this.reset();
-
-  }
-
-  onDelete(id: number) {
-    console.log("hello");
-    const isDelete = confirm("Are you sure you want to delete");
-    if (isDelete) {
-      const index = this.employeeList.findIndex(m => m.employeeId === Number(id));
-      if (index !== -1) {
-        this.employeeList.splice(index, 1);
-        this.onAddNew();
-        localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
-
-        this.employeeList = JSON.parse(localStorage.getItem("EmpData") || "[]");
-
-      } else {
-        alert("Employee not found!");
-      }
-
-    }
-  }
-
-
+export default class App extends TuiPortals {
+    protected open = true;
 }
+
---
+++ src/app/enviroment/enviroment.production.ts
@@ -0,0 +1,9 @@
+export const environment: {
+  appwriteEndpoint: string;
+  appwriteProjectId: string;
+  appwriteProjectName: string;
+} = {
+  appwriteEndpoint: 'https://fra.cloud.appwrite.io/v1',
+  appwriteProjectId: '6917abb30007bedcfeb1',
+  appwriteProjectName: 'Appwrite Project'
+};
